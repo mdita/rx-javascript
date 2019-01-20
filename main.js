a90a06049b7ddb39ceade08a24719d64ff8146fe
Mihai Dita <mihai.dita@metrosystems.net>
2019-01-20 22:47:19 +0200

    my first observable

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 let button = document.getElementById('myButton');
-button.addEventListener('click', (event) => console.log(event));
+// button.addEventListener('click', (event) => console.log(event));
+
+Rx.Observable.fromEvent(button,'click')
+    .throttleTime(2000) // 2 seconds delay here
+    .map((event) => event.screenX) // map only the value we want
+    .subscribe(
+        theValueWeWant => { console.log(theValueWeWant) },
+        error => { console.log(error) }, 
+        () => { console.log('complete') }
+    );
