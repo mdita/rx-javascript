a83320b34f3463a5e72caf18c4e8a35a3b714d11
Mihai Dita <mihai.dita@metrosystems.net>
2019-01-21 07:12:45 +0200

    use observer object

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 let button = document.getElementById('myButton');
 // button.addEventListener('click', (event) => console.log(event));
 
+// Observable created with `fromEvent`
+
+let observer = { // our own observer object
+    next: function(value) {
+        console.log(value);
+    },
+    error: function(error) {
+        console.log(error);
+    },
+    complete: function() {
+        console.log('completed.');
+    }
+};
+
 Rx.Observable.fromEvent(button,'click')
     .throttleTime(2000) // 2 seconds delay here
     .map((event) => event.screenX) // map only the value we want
-    .subscribe(
-        theValueWeWant => { console.log(theValueWeWant) },
-        error => { console.log(error) }, 
-        () => { console.log('complete') }
-    );
+    .subscribe(observer);
+
